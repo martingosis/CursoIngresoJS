f1bbe261419124c8508aa8f3981335edd11d0ec7
martingosis <martin_gosis@hotmail.com>
2017-06-28 22:06:24 -0300

    Add files via upload

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var dato = 0;
+   //dato = "octavio";
+  
+//dato = document.getElementById('elNombre').value;
+//esto me devuelve lo que coloco en el texto
+
+document.getElementById ("elNombre").value = "natalia";
+
+  //alert (dato);
+}
+
+
