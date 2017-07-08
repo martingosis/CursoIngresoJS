6739facf67a7f31bea8f45a5de8e69cea9c08485
martingosis <martin_gosis@hotmail.com>
2017-07-07 22:10:53 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,33 @@
 function Mostrar()
 {
+
+//var mesDelAño = 0;	
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
 
+switch (mesDelAño) {
 
+case "Enero":
+        
+alert ("que comiences bien el año!!!");
+
+        break;
+
+    case "Marzo":
+        
+      alert ("a clases!!!");  
+        
+        break;
+    
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+   
+
+}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,8 +3,33 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño) {
 
+case "Julio":
+
+case "Agosto":
+        
+alert ("Abrigate que hace frio.");
+
+break;
+
+case "Septiembre":
+
+case "Noviembre":
+
+case "Octubre":
+        
+case "Diciembre":
+
+case "Enero":
+
+case "Febrero":
+        
+alert ("Ya pasamos el frio, ahora calor!!");
+
+
+
+}
 
 
 
