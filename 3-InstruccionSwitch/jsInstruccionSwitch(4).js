c6308d3b7111175436ba52e6768786f1ea6be09a
martingosis <martin_gosis@hotmail.com>
2017-07-25 15:16:41 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,9 +3,51 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+
+switch (mesDelAño) {
+
+case "Febrero":
+
+	alert("Este mes tiene 28 dias");
+
+		break;
+
+	case "Enero":
+
+	case "Marzo":
+
+	case "Mayo":
+
+	case "Julio":
+
+	case "Agosto":
+
+	case "Octubre":
+
+	case "Diciembre":
+
+	alert("Este mes tiene 30 o más días");
 	
-	
+		break;
+
+	case "Abril":
+
+	case "Junio":
+
+	case "Septiembre":
+
+	case "Noviembre":
+
+	alert ("Este mes no tiene mas de 30 dias");
+
+	break;	
+
+}	
+
+
+    
+
+    	
 
 
 
