b203efa2e97acc30f6a4d751948c955dc524b516
martingosis <martin_gosis@hotmail.com>
2017-07-25 15:16:20 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota= Math.floor((Math.random() * 10) + 1);
+
+
+alert(nota);
 	
+
+	if(nota>=9)
+	{
+		alert("Excelente");
+	}
+	else
+	{
+		if(nota>4&&nota<9)
+		{
+			alert("Aprobó");
+		}
+		else
+		{
+			alert("Vamos, la proxima se puede");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,12 +1,17 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-	var numero;
+function Mostrar()
+{
+//tomo la edad  
 
- 
-  document.getElementById('FormIngreso').value;
+var numero = Math.floor((Math.random() * 10) + 1);
 
-  Math.floor((Math.random() * 100) + 1);
+alert(numero);
+
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
