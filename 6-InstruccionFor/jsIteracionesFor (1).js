0d5293e9a47a6f7ef9bda3911e198e8b106137e6
martingosis <martin_gosis@hotmail.com>
2017-07-26 22:04:57 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,23 @@
 function Mostrar()
 {
 
+contador=0;
+var respuesta="si";
+
+for (;respuesta=="si";){ 
+
+
+console.log ("hola"+contador);
+
+
+if(contador==10){
+break;
+
+}
+
+contador++;
+respuesta=prompt("deja de salir por el for");
+
+}
+
 }
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,43 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	//var repetciones = prompt("ingrese el número de repeticiones");
+	var cantidad=10000;
+	var contador1=0;
+	var contador2=0;
+	var contador3=0;
+	var contador4=0;
+	var contador5=0;
+	var numero;
 
+	for(contador=0; contador<cantidad; contador ++){
+		numero= Math.floor((Math.random() * 10) + 1);
 
+			switch (numero){
+		
+			case 1:
+			contador1++;
+			break;
+		
+			case 2:
+			contador2++;
+			break;
 
+			case 3:
+			contador3++;
+			break;
+
+			case 4:
+			contador4++;
+			break;
+
+			case 5:
+			contador5++;
+			break;
+
+		}
+
+		console.log(cantidad);
+
+	}
 }//FIN DE LA FUNCIÓN
