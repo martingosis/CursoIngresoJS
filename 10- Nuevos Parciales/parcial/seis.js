2df1906c2ac1921fea63fa67fa1fbe2d2d638b8b
Ulises Merlan <ulimerlan@gmail.com>
2017-07-27 15:57:44 -0300

    puto

--- 10- Nuevos Parciales/parcial/seis.js
+++ 10- Nuevos Parciales/parcial/seis.js
@@ -1,19 +1,32 @@
 function Mostrar()
 {
-	var ventas1 = prompt("ingrese importe");
-
-	var ventas2 = prompt("ingrese importe");
-	
-	
-
-
-
-          
-
-
-	
-
-
-
+  var venta;
+  var input;
+  var count = 0;
+  var min=0;
+  var max=0;
+  
+  while(count<4)
+  {
+    
+    input = prompt("ingrese importe");
+    venta = parseFloat(input);
+    if(venta==0)
+    {
+      alert("No ingresado, igual a cero");
+    }
+    else
+    {
+      
+      if(min>venta || min==0)
+	min=venta;
+      if(max<venta)
+	max=venta;
+      
+      count++;
+    }	
+  }
+        alert("el minimo fue: " + min );
+	alert("el maximo fue: " + max );
 
 }
